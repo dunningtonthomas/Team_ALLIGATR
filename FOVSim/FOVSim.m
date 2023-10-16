8d296d2c6b5b4cbeab07c63dcaea506dd8ee583e
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2023-10-15 20:20:38 -0600

    Update

--- FOVSim/FOVSim.m
+++ FOVSim/FOVSim.m
@@ -48,22 +48,91 @@ dronePositions = [dronePositions1, dronePositions2];
 [~,searchLength] = size(dronePositions);
 
 %% Simulation
+%One iteration is equal to about 0.09583 seconds with an average of 3 m/s
+FOVs = 45:15:160;
+numSims = 1000;
+times1 = zeros(length(FOVs), numSims);
+times2 = zeros(length(FOVs), numSims);
+time1 = 200;
+time2 = 200;
+
+
+for i = 1:length(FOVs)
+    %New FOV
+    radius = range*tand(FOVs(i)/2);
+
+    for k = 1:numSims  %1000 sims for a given FOV
+        %Initial RGV Position
+        RGVPos1 = [bound*rand(1); bound*rand(1)]; %Random initial position
+        RGVPos2 = [bound*rand(1); bound*rand(1)]; %Random initial position
+        
+        %Detection variables
+        detect1 = false;
+        detect2 = false;
+        j = 1;
+        while(~detect1 || ~detect2) %Loop if an RGV hasn't been detected yet
+            %Calculate a new Drone position
+            newDrone = dronePositions(:, j);
+        
+            %Calculate difference between drone position and RGV position
+            diff1 = norm(RGVPos1 - newDrone);
+            diff2 = norm(RGVPos2 - newDrone);
+        
+            %Check if it is within the FOV
+            if(diff1 <= radius && ~detect1)
+                detect1 = true;
+                time1 = j;
+            end
+        
+            if(diff2 <= radius && ~detect2)
+                detect2 = true;
+                time2 = j;
+            end
+        
+            %Update the iteration
+            j = j + 1;
+            if(j > searchLength)
+                break;
+            end
+        end
+        
+        %Save the times
+        times1(i, k) = time1 * 0.09583;
+        times2(i, k) = time2 * 0.09583;
+    end
+end
+
+
+%Calculate the average values for each fov
+time1Avg = mean(times1, 2);
+time2Avg = mean(times2, 2);
+overallAvg = ((time1Avg > time2Avg) .* time1Avg) + ((time2Avg > time1Avg) .* time2Avg);
+
+%% Plotting Simulation
+%FOV circle
+radius = range*tand(45);
+FOVx = radius * cos(angles) + center(1);
+FOVy = radius * sin(angles) + center(2);
+
 %Initial Drone position
-figure();
-fovPlot = plot(FOVx, FOVy, 'linewidth', 2);
+fig1 = figure();
+fovPlot = fill(FOVx, FOVy, 'b', 'FaceAlpha', 0.2, 'EdgeColor','none');
 hold on;
 grid on;
-RGVPlot1 = plot(RGVPos1(1), RGVPos1(2), '.', 'color', 'g');
-RGVPlot2 = plot(RGVPos2(1), RGVPos2(2), '.', 'color', 'g');
+RGVPlot1 = plot(RGVPos1(1), RGVPos1(2), '.', 'color', 'r');
+RGVPlot2 = plot(RGVPos2(1), RGVPos2(2), '.', 'color', 'r');
 ylim([-25 bound + 25]);
 xlim([-25 bound + 25]);
 
 %Draw the boundary
 boundary = [0, bound, bound, 0, 0; 0, 0, bound, bound, 0];
-plot(boundary(1,:), boundary(2,:), 'linewidth', 1, 'color', 'k', 'LineStyle','--');
+boundPlot = plot(boundary(1,:), boundary(2,:), 'linewidth', 1, 'color', 'k', 'LineStyle','--');
+
+title('RGV Detection Simulation');
+xlabel('X Distance (ft)');
+ylabel('Y Distance (ft)');
 
 %Simulation Loop
-detect = false;
 iteration = 1;
 while(true)
 
@@ -107,13 +176,25 @@ while(true)
     %Update the iteration of the search algorithm
     iteration = iteration + 1;
     if(iteration > searchLength)
-        iteration = 1; %Restart the algorithm
+        break;
     end
 
     %Sleep
     %pause(tStep/50);
+    movieVec(iteration) = getframe(fig1);
 end
 
+
+
+%% Save the animation
+% %Save animation to a file
+movieVec = movieVec(2:2500);
+vidWrite = VideoWriter('FOV_Sim');
+vidWrite.FrameRate = 100;
+
+open(vidWrite);
+writeVideo(vidWrite, movieVec);
+close(vidWrite);
 
 
 
