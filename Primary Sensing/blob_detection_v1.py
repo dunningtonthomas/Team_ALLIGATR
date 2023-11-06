5f13cb968a57c5d0522fff4497e04086f8468fe0
Yarden Kelmann <yake5628@colorado.edu>
2023-11-05 18:49:35 -0700

    updated for video feed

--- Primary Sensing/blob_detection_v1.py
+++ Primary Sensing/blob_detection_v1.py
@@ -1,30 +1,58 @@
 import numpy as np
 import cv2
+import matplotlib as mpl
 import os
 import time
 
-start = time.time()
 
+def detectBlob(image):
+    start = time.time()
+    # Detect blobs. Filter by color
+    #result = image.copy()
+    #image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    #lower = np.array([0,0,0])
+    #upper = np.array([100,100,100])
+    #mask = cv2.inRange(image, lower, upper)
+    #result = cv2.bitwise_and(result, result, mask=mask)
+
+    #cv2.imshow('mask', mask)
+    #cv2.imshow('result', result)
+    #cv2.waitKey()
+
+    color_low = (0,0,0)
+    color_high = (255,255,255)
+    mask = cv2.inRange(image,color_low,color_high)
+    #cv2.imshow('mask',mask)
+    #cv2.waitKey(0)
+    # Image detector
+    keypoints = detector.detect(image)
+
+    centroids_x = np.array([])
+    centroids_y = np.array([])
+    for keypoint in keypoints:
+        centroids_x = np.append(centroids_x, keypoint.pt[0])
+        centroids_y = np.append(centroids_y, keypoint.pt[1])
+
+    centroids_x = centroids_x.astype(int)
+    centroids_y = centroids_y.astype(int)
+
+    time_elapsed = (time.time() - start)
+    print(time_elapsed)
+    return zip(centroids_x,centroids_y)
+
+def drawCentroids(image, centroids):
+    im_with_centroids = image
+    for centroid_x,centroid_y in centroids:
+        im_with_centroids = cv2.circle(im_with_centroids, (centroid_x,centroid_y), radius=0, color=(0,0,255), thickness=20)
+    return im_with_centroids
 
 # Read image
 #im = cv2.imread(r"c:\Users\ykelm\Autonomous-AerialLocalizationTeam1\Primary Sensing\square_in_field.png",1)
-im = cv2.imread("square_in_field.png",1)
+im = cv2.imread(r"c:\Users\ykelm\Autonomous-AerialLocalizationTeam1\ARTags\Markers\ExampleMarker.png",1)
 
 # Read video feed
 #vid = cv2.VideoCapture(r"c:\Users\ykelm\Autonomous-AerialLocalizationTeam1\Primary Sensing\sample_vid.mp4",1)
-'''
-while(1):
-    print("in here")
-    ret, frame = vid.read()
-
-    cv2.imshow('frame',frame)
-    cv2.waitKey(25)
-
-vid.release()
-#cv2.destroyAllWindows()    
-'''       
-   
-
+         
 # Setup SimpleBlobDetector parameters.
 params = cv2.SimpleBlobDetector_Params()
 
@@ -55,45 +83,34 @@ params.blobColor = 0
 detector = cv2.SimpleBlobDetector_create(params)
 
 
-# Detect blobs. Filter by color
-color_low = (0,0,0)
-color_high = (255,255,255)
-mask = cv2.inRange(im,color_low,color_high)
-
-# Image detector
-keypoints = detector.detect(im)
-
-centroids_x = np.array([])
-centroids_y = np.array([])
-for keypoint in keypoints:
-    centroids_x = np.append(centroids_x, keypoint.pt[0])
-    centroids_y = np.append(centroids_y, keypoint.pt[1])
-
-centroids_x = centroids_x.astype(int)
-centroids_y = centroids_y.astype(int)
-
-end = time.time()
-print(end - start)
+centroid_coords = detectBlob(im)
 
 # Draw centroids of detected blobs
-im_with_centroids = im
-for centroid_x,centroid_y in zip(centroids_x,centroids_y):
-    im_with_centroids = cv2.circle(im_with_centroids, (centroid_x,centroid_y), radius=0, color=(0,255,0), thickness=5)
+im_with_centroids = drawCentroids(im,centroid_coords)
 
 # Show blobs
 cv2.imshow("Centroids", im_with_centroids)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
-'''
 ## Video detector
-#while(vid.isOpened()):
-    
-#vid.release()
+vid = cv2.VideoCapture(0)
 
-_,frame = vid.read()
-cv2.imshow
-'''
+while(vid.isOpened()):
+    _,frame = vid.read()
+    centroids = detectBlob(frame)
+    im_with_centroids = drawCentroids(frame,centroids)
+    cv2.imshow('frame',im_with_centroids)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+  
+# After the loop release the vid object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows()
+
 
 
 
