39ad2228046dc2a454482bb6802483b488f82bf7
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2023-11-10 13:37:27 -0700

    Update Localize.py

--- ARTags/Localize.py
+++ ARTags/Localize.py
@@ -29,6 +29,7 @@ ARUCO_DICT = {
 	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
 }
 
+# %% Functions
 # Highlight the detected markers
 def aruco_display(corners, ids, rejected, image):
     if(len(corners) > 0): # Are any aruco tags detected
@@ -43,45 +44,102 @@ def aruco_display(corners, ids, rejected, image):
             bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
             topLeft = (int(topLeft[0]), int(topLeft[1]))
 
+            # Centroid
+            xf = round((topLeft[0] + bottomRight[0]) / 2)
+            yf = round((topLeft[1] + bottomRight[1]) / 2)
+
+            # Image is 1920 by 1080 pixels
+            xPixels = 1920
+            yPixels = 1080
+
+            # Define the center of the image
+            cx = int(xPixels / 2)
+            cy = int(yPixels / 2)
+
             # Draw the lines for the AR tag detection
             cv2.line(image, topLeft, topRight, (0, 0, 255), 2)
             cv2.line(image, topRight, bottomRight, (0, 0, 255), 2)
             cv2.line(image, bottomRight, bottomLeft, (0, 0, 255), 2)
             cv2.line(image, bottomLeft, topLeft, (0, 0, 255), 2)
+            cv2.circle(image, (xf,yf), radius=0, color=(0, 0, 255), thickness=10)
+
+            # Draw principle point and line from principle point to the centroid
+            cv2.circle(image, (cx, cy), radius=0, color=(0, 255, 0), thickness=10)
+            cv2.line(image, (cx, cy), (xf,yf), (0, 255, 0), 2)
+
+            # Draw Line from principle point forward along the drone x-axis
+            cv2.line(image, (cx, cy), (cx, cy - 100), (0, 255, 0), 2)
+            
+            # Resize the image
+            image = cv2.resize(image, (1280, 720))                # Resize image for display
+            
 
     return image
 
+def localize(corners, height, ids):
+
+    # Check if ids is empty
+    if ids is None:
+        return 0, 0
+
+    # Image is 1920 by 1080 pixels
+    xPixels = 1920
+    yPixels = 1080
+
+    # Field of view, standard values for our wide fov camera
+    HFOV = 100 * np.pi / 180 
+    VFOV = 70 * np.pi / 180 
+
+    # Define the center of the image
+    cx = xPixels / 2
+    cy = yPixels / 2
+
+    # Define the focal length and principal point translation, this is the intrinsic camera matrix
+    f = 2.87
+
+    # Get the centroid coordinates of the AR tag
+    firstCorners = corners[0][0]
+    topLeft = firstCorners[0]
+    bottomRight = firstCorners[2]
+
+    xf = (topLeft[0] + bottomRight[0]) / 2
+    yf = (topLeft[1] + bottomRight[1]) / 2
+
+
+    # Using the center, find the relative position of the RGV
+    # Compute the 2D normalized coordinates with respect to the intrinsic camera matrix
+    dx = f * np.tan(HFOV / 2)
+    dy = f * np.tan(VFOV / 2)
+
+    # Scaling factors
+    sx = cx / dx
+    sy = cy / dy
+
+    # Calcaulte the depth of the principal point projection on the ground
+    Zp = height    
+
+    # Distance calculation
+    d = np.sqrt(((cx - xf) / sx)**2 + ((cy - yf) / sy)**2)
+    theta = np.arctan(d / f)
+    Z = Zp / np.cos(theta)
+
+    # Bearing calculation
+    p = np.arctan2((xf - cx), (cy - yf))   
+
+    # Range and bearing output
+    range = Z
+    bearing = p * 180 / np.pi   # Deg
+
+    # Return the range and bearing measurements
+    return range, bearing
+
+
+
+# %% Main
 # Dictionary
 aruco_type = "DICT_6X6_250"
 testDict = cv2.aruco.getPredefinedDictionary(ARUCO_DICT[aruco_type])
 arucoParams = cv2.aruco.DetectorParameters()
-
-# Path for aruco tags
-# path = "ARTags/Markers/"
-
-# # Get predefined dictionary
-
-# # Insantiate parameters
-
-
-# # Read in the image
-# imagePath = path + "ExampleMarker.png"
-# img = cv2.imread(imagePath, cv2.IMREAD_COLOR)
-
-# # Detect the marker
-# corners, ids, rejected = cv2.aruco.detectMarkers(img, testDict, parameters=arucoParams)
-
-# # Draw the detection
-# image = aruco_display(corners, ids, rejected, img)
-
-# # Display the image with the detected AR tag
-# cv2.imshow("ArUco Marker", image)
-
-# # Wait until a key is pressed
-# cv2.waitKey(0)
-
-# # Close all of the windows
-# cv2.destroyAllWindows()
 
 
 # %% Video Capture With Webcam
@@ -89,14 +147,15 @@ path = "ARTags/Videos/"
 videoPath = path + "AR_Tag_Test_Trim.mp4"
 cap = cv2.VideoCapture(videoPath)
 
-#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+# Assume constant height
+height = 30
 
 while cap.isOpened():
-    print("test1")
     # Get the current video feed frame
     ret, img = cap.read()
-    print("test2")
     
 	# Check if the image is empty
     if img is None:
@@ -104,13 +163,23 @@ while cap.isOpened():
     
     # Locate the Aruco tag
     corners, ids, rejected = cv2.aruco.detectMarkers(img, testDict, parameters=arucoParams)
-    print("test3")
     image = aruco_display(corners, ids, rejected, img)
-    print("test4")
     
 	# Display the frame
     cv2.imshow('frame', image)
-    print("test5")
+
+    # Output the range and bearing
+    range, bearing = localize(corners, height, ids)
+
+    # Print results
+    if range == 0 and bearing == 0:
+        continue
+
+    print("Range: ", range)
+    print("Bearing: ", bearing)
+
+    # Wait until a key is pressed
+    cv2.waitKey(0)
     
 	# Quit
     key = cv2.waitKey(1) & 0xFF
@@ -122,73 +191,6 @@ cv2.destroyAllWindows()
 cap.release()	
 
 
-
-# %% Attempt to localize
-
-# Image is 1280 by 720 pixels
-xPixels = 1280
-yPixels = 720
-
-# Field of view, standard values for our wide fov camera
-HFOV = 120 * np.pi / 180 # (RAD)
-VFOV = 70 * np.pi / 180 
-
-# Define the center of the image
-cx = xPixels / 2
-cy = yPixels / 2
-
-# Define the focal length and principal point translation, this is the intrinsic camera matrix
-f = 2.87 * 0.00328084	# Focal length in mm to ft
-fx = f
-fy = f
-
-# Define the height and angle of the camera
-H = 30  # ft
-alpha = 0   # Camera is fixed to the drone frame looking straight down
-
-# Get the centroid coordinates of the AR tag
-firstCorners = corners[0][0]
-topLeft = firstCorners[0]
-bottomRight = firstCorners[2]
-
-xf = (topLeft[0] + bottomRight[0]) / 2
-yf = (topLeft[1] + bottomRight[1]) / 2
-
-print("Center of AR Tag (Pixels): ", xf, " ", yf)
-
-# Using the center, find the relative position of the RGV
-
-# Compute the 2D normalized coordinates with respect to the intrinsic camera matrix
-xn = (xf - cx) / fx
-yn = (yf - cy) / fy
-
-# Calcaulte the depth of the principal point projection on the ground
-Zp = H / np.cos(alpha)
-
-# Angles from the field of view
-vertAng = VFOV / 2
-horzAng = HFOV / 2
-
-# Number of pixels
-dx = f * np.tan(horzAng)
-dy = f * np.tan(vertAng)
-
-# Scaling factors
-sx = cx / dx
-sy = cy / dy
-
-# Distance calculation
-d = np.sqrt(((cx - xf) / sx)**2 + ((cy - yf) / sy)**2)
-theta = np.arctan(d / f)
-Z = Zp / np.cos(theta)
-
-# Camera frame coordinates
-xc = xn * Z
-yc = yn * Z
-zc = Z
-
-# Print results
-print("Camera frame coordinates: ", "X: ", xc, "Y: ", yc, "Z: ", zc)
 
 
 
