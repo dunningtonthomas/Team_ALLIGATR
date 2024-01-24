d7a34f095be2f9ea2073afd267b82f5e99c21cff
ckohl10 <carson.kohlbrenner@gmail.com>
2024-01-24 11:02:38 -0700

    Skeleton

--- HUB/main.cpp
+++ HUB/main.cpp
@@ -3,33 +3,36 @@
 #include "headers/CORE.h"
 
 /*
-$$$$$$$$$$$$$$$$ TO-DO and STRUCTURE $$$$$$$$$$$$$
+TO-DO and STRUCTURE:
+    CAMERA (CLASS)
+        -Object that can adjust focus, record, turn off and on, and take pictures, etc...
 
-CAMERA (CLASS)
-    -Object that can adjust focus, record, turn off and on, and take pictures, etc...
+    WIFI (CLASS)
+        -A check for wifi connection and return speed?
+        -Does a send/recieve function need to be written or can we assume that ROS will handle this?
 
-WIFI (CLASS)
-    -A check for wifi connection and return speed?
-    -Does a send/recieve function need to be written or can we assume that ROS will handle this?
+    MANUAL CONTROLLER
+        -The controller plugs directly into the cube. We need a way to switch priority from the nano to the manual controller. 
+        If this doesn't work, we can try to send manual controlls to the nano directly.
 
-MANUAL CONTROLLER
-    -The controller plugs directly into the cube. We need a way to switch priority from the nano to the manual controller. 
-    If this doesn't work, we can try to send manual controlls to the nano directly.
-
-CUBE (CLASS)
-    - Establish connection to the cube, send waypoints, and recieve data from the cube.
+    CUBE (CLASS)
+        - Establish connection to the cube, send waypoints, and recieve data from the cube.
 */
+
+int main(){
 
 // $$$$$$$$$$$$$ PRECHECK LIST $$$$$$$$$$$$$$$$
     //WIFI CONNECTION
 
     //ROS CONNECTION
 
-    //SET MODE
+    //MANUAL CONTROLLER CONNECTION
 
-        //MANUAL CONTROLLER CONNECTION
+    //CUBE CONNECTION
 
-        //CUBE CONNECTION
+    //CAMERA CONNECTION
 
-        //CAMERA CONNECTION
+    //Wait until mode set to auto
 
+    return 0;
+}
