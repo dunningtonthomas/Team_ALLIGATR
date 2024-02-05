27d1622a1c95176ab2ee0bd040a0c1802d63322d
ckohl10 <carson.kohlbrenner@gmail.com>
2024-02-05 15:28:28 -0700

    Working HUB

--- HUB/engage.sh
+++ HUB/engage.sh
@@ -21,12 +21,30 @@ catkin_make
 source devel/setup.bash
 cd ${CURRENT_DIR} #Go back to the build directory
 
+run_node()
+{
+    PKG_NAME=$1
+    NODE_NAME=$2
+
+    # Make sure the node being ran is an executable
+    cd ${CURRENT_DIR}/ros_packages/${PKG_NAME}
+    chmod +x ${NODE_NAME}
+    cd ${CURRENT_DIR}
+
+    # Launch the XTERM terminal and run the ROS node
+    # Make copies of this line of code for any additional nodes
+    xterm -e "source ~/.bashrc; rosrun $PKG_NAME $NODE_NAME; exec bash" &
+}
+
 #Opens a roscore terminal. If one already exists, it will close itself
 xterm -e "source ~/.bashrc; roscore; exit; exec bash" &
 
-# Launch the XTERM terminal and run the ROS node
-# Make copies of this line of code for any additional nodes
-xterm -e "source ~/.bashrc; rosrun ros_beginner_package first_node.py; exec bash" &
+
+################### ADD PROCESSES HERE ######################
+
+run_node ros_beginner_package first_cpp_node.cpp
+
+#############################################################
 
 #Make the project using the passed in source file
 cmake -DSOURCE_FILE=${SOURCE_FILE} ..
